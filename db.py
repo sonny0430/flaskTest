9d0be5246ce5f789484511427b8ad93469e8e49d
SonSG <157447884+sonny0430@users.noreply.github.com>
2026-03-25 15:49:05 +0900

    Update db.py

--- db.py
+++ db.py
@@ -6,9 +6,10 @@ class Database:
         self.connection = None
         try:
             self.connection = pymysql.connect(
-                # host='mariadb',
                 # host='svc.sel5.cloudtype.app',
                 # port=3308,
+                host='mariadb',
+                port=3306,
                 database='test',  # test 데이터베이스 사용
                 user='root',
                 password='sonsk0430',  # mariadb 설치 당시의 패스워드, 실제 환경에서는 보안을 위해 환경변수 등을 사용
